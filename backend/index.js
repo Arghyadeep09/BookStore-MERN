a42e33fbda8238df2932325cb907619a7d6e7f36
Arghya <arghyahari2001@gmail.com>
2024-04-29 00:11:11 +0530

    new modified

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,36 @@
 import express from 'express';
 import {PORT , mongodbURL} from "./config.js"
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import bookRoute from './routes/bookRoute.js';
+import cors from 'cors';
 
 const app=express();
+
+app.use(express.json());
+
+app.use(cors());
+//Middleware for handling CORS POLICY  
+
+// app.use(
+//     cors({
+//         origin:"http://localhost:3000",
+//         methods:['POST','GET','PUT','DELETE'],
+//         allowedHeaders:['contet-type'],
+//     })
+// );
+
 
 app.get('/',(request,response)=>{
     console.log(request);
     return response.status(200).send('Welcome to Project of mern')
 })
 
-
+app.use('/books',bookRoute);
 
 mongoose.connect(mongodbURL).then(()=>{
     console.log(`App connected to database `);
-    app.listen(PORT,()=>{
+    app.listen(PORT,()=>{ 
         console.log(`Server listening on port ${PORT}`);
     })
 })
---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowBook = () => {
+  return (
+    <div>
+      showbook
+    </div>
+  )
+}
+
+export default ShowBook
